04e0cdcd7a53ed1fb34811468cfb83cbad84069b
Maia Borghese <maiaborghese@gmail.com>
2019-11-09 16:06:40 -0800

    completed

--- search.js
+++ search.js
@@ -4,36 +4,46 @@
 
 var appendArticleAttributes = function(article) {
 
-  console.log(article);
-
   var title = document.createElement('div');
   var section = document.createElement('div');
   var byline = document.createElement('div');
+  var link = document.createElement('a');
+
+
 
   var li = document.createElement('li');
 
-	title.textContent = article.title;
-  section.textContent = article.section;
-  byline.textContent = article.byline;
+	title.textContent = "Title: " + article.title;
+  section.textContent = "Section: " + article.section;
+  byline.textContent = "Byline: " + article.byline;
+  link.textContent = "Link";
+  link.href = article.url;
 
   li.appendChild(title);
   li.appendChild(section);
   li.appendChild(byline);
+  li.appendChild(link);
 
   list.appendChild(li);
 }
 
+var makeRequest = function() {
 
-var search = function () {
-  console.log("search clicked");
+  var searchTerm = document.getElementById("searchBox").value;
+
   var request = new XMLHttpRequest();
 
   request.open('GET', 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=Gwxln5M3geWlhR6UE0TY1FUWKSG3wCil');
 
   request.onload = function() {
     if (this.status >= 200 && this.status < 400) {
-      var articles = JSON.parse(this.response).results;
-      console.log(_.map(articles, appendArticleAttributes));
+      var results = JSON.parse(this.response).results;
+      var titleMatch = _.filter(results, _.conforms({ 'title': function(title) { return title.toUpperCase().includes(searchTerm.toUpperCase()); } }));
+      var bylineMatch = _.filter(results, _.conforms({ 'byline': function(byline) { return byline.toUpperCase().includes(searchTerm.toUpperCase()); } }));
+      var sectionMatch = _.filter(results, _.conforms({ 'section': function(section) { return section.toUpperCase().includes(searchTerm.toUpperCase()); } }));
+
+      list.innerHTML = '';
+      _.map(_.concat(titleMatch, bylineMatch, sectionMatch), appendArticleAttributes);
     }
     else {
      console.log(this);
@@ -47,8 +57,16 @@ var search = function () {
   request.send();
 }
 
+
+
 window.addEventListener('load', function() {
+
   var searchButton = document.getElementById("searchButton");
   var articleList = document.getElementById("list");
-  searchButton.addEventListener('click', search);
+  searchButton.onclick = makeRequest.bind(this);
+
+    document.getElementById('searchForm').addEventListener('submit', function(e) {
+      makeRequest();
+      e.preventDefault();
+    }, false);
 })
